f68d59a4f52854a66c3adb07806def08c80a4f87
JMWalkman <josemanuel.tmn@gmail.com>
2023-01-04 21:17:20 -0500

    renderizar vistas funciona con la base de datos conectadagit add .!

--- models/Index.js
+++ models/Index.js
@@ -1,17 +1,38 @@
-const Product = require('./Product.js');
-const Category = require('./Category.js');
-const User = require('./User.js');
-const Features = require('./Features.js');
-const Manufacturers = require('./Manufacturers.js');
+'use strict';
 
-Product.belongsTo(Category, {foreignKey: 'category_id'});
-Product.belongsTo(Features, {foreignKey: 'features_id'});
-Product.belongsTo(Manufacturers, {foreignKey: 'manofacturer_id'});
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
+const db = {};
 
-module.exports = {
-    Product,
-    Category,
-    User,
-    Features,
-    Manufacturers
-  };
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
 const express = require('express');
-const app = express();
+const app = express()
 const path = require('path');
 const session = require('express-session');
-const cookies = require('cookie-parser')
-const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware')
-const db = require('../config/db.js')
+const cookies = require('cookie-parser');
+const methodOverride =  require('method-override'); // Para poder usar los métodos PUT y DELETE
 
-try {
-    db.authenticate();
-    db.sync();
-    console.log('Conexion correcta a la base datos');
-} catch (error) {
-    console.log(error);
-};
+const mainRouter = require('./routes/main');
+// const productRouter = require('./routes/product');
+// const cartRouter = require('./routes/cart');
+const userRouter = require('./routes/user');
+// const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware')
 
 //cookie-parser
 app.use(cookies())
@@ -23,16 +20,7 @@ app.use(session({
     saveUninitialized: false
 }))
 
-app.use(userLoggedMiddleware)
-
-const mainRouter = require('./routes/main');
-const productRouter = require('./routes/product');
-const cartRouter = require('./routes/cart');
-const userRouter = require('./routes/user');
-const methodOverride =  require('method-override'); // Para poder usar los métodos PUT y DELETE
-
-// const http = require('http');
-// const server = http.createServer((req, res) => {res.end("response ended")})
+// app.use(userLoggedMiddleware)
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); // siempre se debe hacer cuando views no esté en raíz
@@ -41,17 +29,11 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(methodOverride('_method')); // Pasar poder pasar el method="POST" en el formulario por PUT y DELETE
-/* app.use(session({
-    secret: 'Texto indiferente?',
-    resave: false,
-    saveUninitialized: false
-})) */
 
 app.use('/', mainRouter)
-app.use('/products/', productRouter);
-app.use('/cart/', cartRouter);
+// app.use('/products/', productRouter);
+// app.use('/cart/', cartRouter);
 app.use('/users/', userRouter);
-
 
 app.get('/slide.js', (req,res) => res.sendFile(__dirname + '/controllers/sliderController.js')); // Ruta del slider funcionando ! 
 app.use((req, res, next) => {
@@ -60,4 +42,3 @@ app.use((req, res, next) => {
 });
 
 app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
-// Conexion a la base de datos
